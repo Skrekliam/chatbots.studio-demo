da16ba5727ed38cd40b91126ab6c01b6bfc31bae
Skrekliam <typnickman@gmail.com>
2021-07-06 17:57:57 +0300

    Add upload for api request

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,13 @@
 const express: any = require('express');
-
+const axios: any = require('axios');
 const app: any = express();
 
-app.post('/upload/dog/image', (req: any, res: any): any => {
-    res.send(req);
+app.get('/', (req: any, res: any): any => {
+    res.send('Homepage');
 })
+
+app.get('/upload/dog/image', (req: any, res: any): any => {
+    axios.get('https://random.dog/woof.json').then((res1 : any)=> console.log(res1.data))
+})
+
+app.listen(3000, (): any => console.log('port : 3000'));
